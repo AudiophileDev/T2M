c72fe55b138a7543b6812f8f3078c813b708e46d
daniel-scholz <daniel.scholz@online.de>
2017-09-05 11:49:15 +0200

    Created image folder

--- src/com/audiophile/t2m/Main.java
+++ src/com/audiophile/t2m/Main.java
@@ -2,6 +2,6 @@ package com.audiophile.t2m;
 
 public class Main{
     public static void main(String[]  args){
-        System.out.println("Hello Music! I ams T2M!");
+        System.out.println("Hello Text2Music!");
     }
 }
