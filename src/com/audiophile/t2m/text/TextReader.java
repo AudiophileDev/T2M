d83896113020d7710b46a6bace63216ec2fbf573
Tobi <tobi.wymer@googlemail.com>
2017-09-06 12:59:10 +0200

    TextReader Created  - Removed TextReader from Main and created seperate class

---
+++ src/com/audiophile/t2m/text/TextReader.java
@@ -0,0 +1,53 @@
+package com.audiophile.t2m.text;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Created by Tobi on 06.09.2017.
+ */
+public class TextReader {
+    private String fileContent;
+
+    public TextReader(String inputFile){
+        try {
+            fileContent = readTxtFile(inputFile);
+        } catch (FileNotFoundException e) {
+            System.out.flush();
+            System.err.println(e.getMessage());
+            return;
+        } catch (IOException e) {
+            System.out.flush();
+            System.err.println("Error reading file \"" + inputFile + "\"");
+            return;
+        }
+    }
+
+     /**
+     * Reads a given txt file
+     *
+     * @param fileName String Path to the file
+     * @return String Plain File content
+     * @throws IOException Throws an error if the file was not found or could not be read
+     */
+    private String readTxtFile(String fileName) throws IOException {
+        File file = new File(fileName);
+        if (!file.exists())
+            throw new FileNotFoundException("The file \"" + fileName + "\" does not exist");
+
+        FileInputStream stream = new FileInputStream(file);
+        byte[] data = new byte[(int) file.length()];
+        int fileLength = stream.read(data);
+        stream.close();
+
+        System.out.println("File length: " + fileLength + " bytes");
+        return new String(data, "UTF-8");
+    }
+
+
+    public String getFileContent(){
+        return fileContent;
+    }
+}
