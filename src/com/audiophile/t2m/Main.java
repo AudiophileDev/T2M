3f9e78128651f4fa986298f1b8854791370548b8
daniel-scholz <daniel.scholz@online.de>
2017-09-06 12:39:30 +0200

    Test Push  - Hello World in Main.main()

--- src/com/audiophile/t2m/Main.java
+++ src/com/audiophile/t2m/Main.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World!");
         try {
             validateArguments(args);
         } catch (IllegalArgumentException e) {
