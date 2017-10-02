e834d10533b614bf10b8b4d8c7f07645f50cf274
Simon Niedermayr <simon.niedermayr@gmx.de>
2017-10-02 16:48:30 +0200

    Added file writing interface

---
+++ src/com/audiophile/t2m/writer/MP3Writer.java
@@ -0,0 +1,12 @@
+package com.audiophile.t2m.writer;
+
+import javax.sound.midi.Sequence;
+import java.io.IOException;
+
+public class MP3Writer implements MusicWriter {
+
+    @Override
+    public void writeFile(Sequence channel, String filename) throws IOException {
+        //TODO export mp3 file
+    }
+}
