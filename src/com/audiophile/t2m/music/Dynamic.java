b14aca26d304aed6df1f77a97d3c938395078cb5
daniel-scholz <daniel.scholz@online.de>
2017-11-03 02:58:42 +0100

    removed debug output

--- src/com/audiophile/t2m/music/Dynamic.java
+++ src/com/audiophile/t2m/music/Dynamic.java
@@ -29,7 +29,7 @@ public class Dynamic {
                 return extrema;
             }
         }
-        System.out.println(Arrays.toString(extrema));
+       // System.out.println(Arrays.toString(extrema));
         return extrema;
     }
 
